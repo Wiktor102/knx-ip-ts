e170f101e6e0b405dfe155c5f1e076cbeaf0ce6e
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-09-21 13:36:46 +0200

    Basic HPAI and HPID implementation

---
+++ src/host.ts
@@ -0,0 +1,47 @@
+interface HostProtocol {
+	ip: string;
+	port: number;
+	toBuffer(): Buffer;
+}
+
+class HostProtocolAddressInformation implements HostProtocol {
+	constructor(
+		public ip: string,
+		public port: number
+	) {}
+
+	toBuffer(): Buffer {
+		const buffer = Buffer.alloc(8);
+		buffer.writeUInt8(0x08, 0);
+		buffer.writeUInt8(0x01, 1);
+
+		this.ip.split(".").forEach((v, i) => {
+			buffer.writeUint8(+v, i + 2);
+		});
+
+		buffer.writeUInt16BE(this.port, 6);
+
+		return buffer;
+	}
+
+	static fromBuffer(buffer: Buffer): HostProtocolAddressInformation {
+		const ip = buffer.subarray(2, 6).join(".");
+		const port = buffer.readUInt16BE(6);
+		return new HostProtocolAddressInformation(ip, port);
+	}
+}
+
+class HostProtocolIndependentDevice implements HostProtocol {
+	constructor(
+		public ip: string,
+		public port: number,
+		public mac: string,
+		public individualAddress: string
+	) {}
+
+	toBuffer(): Buffer {
+		return Buffer.alloc(12);
+	}
+}
+
+export { HostProtocolAddressInformation, HostProtocolIndependentDevice };
