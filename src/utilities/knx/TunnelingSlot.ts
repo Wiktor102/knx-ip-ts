da00fb2fbf50d4527c4d5665d44a6ebdd6924481
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-10-13 13:00:02 +0200

    Added a class for handling knx tunneling slots

---
+++ src/utilities/knx/TunnelingSlot.ts
@@ -0,0 +1,23 @@
+import IndividualAddress from "../IndividualAddress.js";
+
+class TunnelingSlot {
+	constructor(
+		public individualAddress: IndividualAddress,
+		public usable: boolean,
+		public authorized: boolean,
+		public free: boolean
+	) {}
+
+	static fromBuffer(buffer: Buffer): TunnelingSlot {
+		if (buffer.length != 4) throw new Error("Expected 4 bytes, but got " + buffer.length);
+
+		const address = IndividualAddress.fromBuffer(buffer.subarray(0, 2));
+		const usable = (buffer[3] & 0b100) != 0;
+		const authorized = (buffer[3] & 0b10) != 0;
+		const free = (buffer[3] & 0b1) != 0;
+
+		return new TunnelingSlot(address, usable, authorized, free);
+	}
+}
+
+export default TunnelingSlot;
