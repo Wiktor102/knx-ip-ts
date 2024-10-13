4783c66ba9e1efbe243222a5c980b8f74b9d84bb
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-10-13 13:00:41 +0200

    Added some helper types (forgot to commit them earlier)

--- src/utilities/types/helpers.ts
+++ src/utilities/types/helpers.ts
@@ -1,12 +1,12 @@
-import Response from "../../messages/Response.js";
 import { IStructureConstructor } from "../../structures/Structure.js";
+import Response from "../../messages/Response.js";
 
 type InstanceTypeOf<T> = T extends new (...args: any[]) => infer R ? R : never;
 type MapTupleToInstances<T extends readonly any[]> = {
 	[K in keyof T]: InstanceTypeOf<T[K]>;
 };
 
-type ResponseConstructor = new (chunks: any) => Response;
+type ResponseConstructor = new (chunks: any, rest?: Buffer | null) => Response;
 type ResponseConstructorTupleItem = IStructureConstructor | null;
 type ChunksTuple<T extends readonly any[] = readonly any[]> = {
 	[K in keyof T]: T[K] extends ResponseConstructorTupleItem ? T[K] : never;
