0f88fa629ebfc8abd99fe941b15890ddd83dfa1c
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-10-14 18:14:09 +0200

    KnxSocket refactor

--- src/socket/KnxSocket.ts
+++ src/socket/KnxSocket.ts
@@ -26,6 +26,7 @@ class KnxSocket extends Listenable<IKnxSocketEvent> {
 	protected server: HostProtocolAddressInformation;
 	protected client: HostProtocolAddressInformation;
 	protected socket: udp.Socket;
+	#socketReady: boolean = false;
 
 	constructor(options: IKnxSocketOptions) {
 		super();
@@ -35,9 +36,10 @@ class KnxSocket extends Listenable<IKnxSocketEvent> {
 		this.socket = udp.createSocket("udp4");
 		this.socket.bind(this.client.port);
 
-		this.socket.on("listening", () => {
+		this.socket.once("listening", () => {
 			this.socket.setBroadcast(true);
 			this.dispatchEvent("ready", this.socket);
+			this.#socketReady = true;
 		});
 
 		this.socket.on("message", msg => {
@@ -50,57 +52,105 @@ class KnxSocket extends Listenable<IKnxSocketEvent> {
 		});
 	}
 
+	get socketReady(): boolean {
+		return this.#socketReady;
+	}
+
+	get port(): number {
+		if (!this.#socketReady) throw new Error("Cannot access port before the socket is ready");
+		return this.socket.address().port;
+	}
+
+	ready(): Promise<void> {
+		return new Promise<void>(resolve => {
+			if (this.#socketReady) {
+				resolve();
+				return;
+			}
+
+			this.addEventListener("ready", () => {
+				resolve();
+			});
+		});
+	}
+
 	send(payload: Buffer | Request) {
 		if (payload instanceof Request) payload = payload.payload;
 
 		this.socket.send(payload, this.server.port, this.server.ip);
 	}
 
-	receive<T>(responseType: any, timeout?: number): Promise<T> {
-		return new Promise<T>((resolve, reject) => {
-			if (timeout) {
-				var errorTimeout = setTimeout(() => {
-					reject(new Error("Timeout"));
-				}, timeout);
-			}
+	receive<T>(responseType: new (...args: any[]) => T, timeout?: number | Promise<void>): Promise<T> {
+		const { promise, resolve, reject } = Promise.withResolvers<T>();
+		let settled = false;
 
-			const listenerId = this.addEventListener("message", (msg: Response) => {
-				if (msg instanceof responseType) {
-					clearTimeout(errorTimeout);
-					this.removeEventListener("message", listenerId);
-					resolve(msg as T);
-				}
+		if (timeout && typeof timeout === "number") {
+			var errorTimeout = setTimeout(() => {
+				if (settled) return;
+
+				reject(new Error("Timeout"));
+				settled = true;
+			}, timeout * 1000);
+		}
+
+		if (timeout instanceof Promise) {
+			timeout.then(() => {
+				if (settled) return;
+				this.removeEventListener("message", listenerId);
+
+				reject(new Error("Timeout"));
+				settled = true;
 			});
+		}
+
+		const listenerId = this.addEventListener("message", (msg: Response) => {
+			if (msg instanceof responseType) {
+				errorTimeout && clearTimeout(errorTimeout);
+				this.removeEventListener("message", listenerId);
+
+				resolve(msg as T);
+				settled = true;
+			}
 		});
+
+		return promise;
 	}
 
+	/**
+	 * @param timeout In seconds
+	 * @return [response, cancel]
+	 */
 	async *receiveAll<T>(
 		responseType: new (...args: any[]) => T,
 		timeout: number,
 		callback?: () => void
-	): AsyncGenerator<T> {
+	): AsyncGenerator<[T, () => void]> {
 		let listening = true;
+		let { promise: cancelReceive, resolve: cancel } = Promise.withResolvers<void>();
 
-		const timeoutId = setTimeout(() => {
+		function cleanUp() {
 			listening = false;
+			cancel();
 			callback && callback();
-		}, timeout);
+		}
+
+		const cleanUpTimeout = setTimeout(cleanUp, timeout * 1000);
 
 		try {
 			while (listening) {
-				const response = await this.receive<T>(responseType, timeout);
-				if (listening) {
-					yield response;
-				}
+				const response = await this.receive<T>(responseType, cancelReceive);
+				if (listening) yield [response, cancel];
 			}
 		} catch (e) {
 			// We ignore all promise rejections
 		} finally {
-			clearTimeout(timeoutId);
+			clearTimeout(cleanUpTimeout);
+			cleanUp();
 		}
 	}
 
 	close() {
+		this.#socketReady = false;
 		this.socket.close();
 		this.clearListeners();
 	}
